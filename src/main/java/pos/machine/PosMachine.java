69b9f7903ede5d443834423fdd59b630aadfac60
Travis You <1244098000@qq.com>
2022-07-19 20:37:57 +0800

    feat: implement the function 'countQuantity' check

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,11 +1,17 @@
 package pos.machine;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PosMachine {
     public Map<String, Integer> countQuantity(List<String> barcodes) {
-        return null;
+        Map<String, Integer> barcodesQuantity = new HashMap<String, Integer>(0);
+        for (String barcode: barcodes) {
+            int nowQuantity = barcodesQuantity.containsKey(barcode) ? barcodesQuantity.get(barcode) : 0;
+            barcodesQuantity.put(barcode, nowQuantity + 1);
+        }
+        return barcodesQuantity;
     }
 
     public Receipt makeReceipt(Map<String, Integer> barcodesQuantity) {
@@ -33,6 +39,8 @@ public class PosMachine {
     }
 
     public String printReceipt(List<String> barcodes) {
-        return null;
+        Map<String, Integer> barcodesQuantity = countQuantity(barcodes);
+        Receipt receipt = makeReceipt(barcodesQuantity);
+        return renderReceipt(receipt);
     }
 }
